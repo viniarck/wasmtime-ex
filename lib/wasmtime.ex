fb3e32d769f30acf716cf93a1c63e0396b5f05b9
Vinicius Arcanjo <viniarck@gmail.com>
2020-10-11 16:11:22 -0300

    Updated wasmtime.ex accordingly

--- lib/wasmtime.ex
+++ lib/wasmtime.ex
@@ -10,12 +10,12 @@ defmodule Wasmtime do
 
   @impl true
   def init(payload = %FromBytes{}) do
-    {:ok, Map.put(payload, :id, System.unique_integer())}
+    {:ok, Map.put(payload, :id, System.unique_integer([:monotonic]))}
   end
 
   @impl true
   def init(payload = %FromFile{}) do
-    {:ok, Map.put(payload, :id, System.unique_integer())}
+    {:ok, Map.put(payload, :id, System.unique_integer([:monotonic]))}
   end
 
   @impl true
@@ -23,21 +23,38 @@ defmodule Wasmtime do
     {:reply, Native.func_call(payload.id |> Integer.to_string(), fn_name, params), payload}
   end
 
+  def func_imports_to_term(func_imports) do
+    case length(func_imports) do
+      0 ->
+        []
+
+      _ ->
+        [
+          {System.unique_integer([:positive]), func_imports |> hd |> elem(1),
+           func_imports |> hd |> elem(2)}
+        ]
+    end
+  end
+
   @impl true
   def handle_call({:load}, _from, payload) do
     case payload do
       payload = %FromBytes{} ->
         {:reply,
-         Native.load_from_bytes(
+         Native.load_from(
            payload.id |> Integer.to_string(),
-           payload.bytes |> :binary.bin_to_list()
+           "",
+           payload.bytes |> :binary.bin_to_list(),
+           payload.func_imports |> func_imports_to_term
          ), payload}
 
       payload = %FromFile{} ->
         {:reply,
-         Native.load_from_file(
+         Native.load_from(
            payload.id |> Integer.to_string(),
-           payload.file_path
+           payload.file_path,
+           [],
+           payload.func_imports |> func_imports_to_term
          ), payload}
     end
   end
@@ -53,8 +70,20 @@ defmodule Wasmtime do
   end
 
   @impl true
-  def handle_info({:call_back, msg}, payload) do
-    IO.inspect("call_back #{msg}")
+  def handle_info({:call_back, id, param}, payload) do
+    IO.inspect("call_back #{id} #{param}")
+
+    # TODO parametrize accordingly
+    # if length(payload.func_imports) == 1 do
+    #   IO.inspect(Enum.at(payload.func_imports, 0).())
+    # end
+
+    {:noreply, payload}
+  end
+
+  @impl true
+  def handle_info({:call_back, id, param, param2}, payload) do
+    IO.inspect("call_back2 #{id} #{param} #{param2}")
 
     # TODO parametrize accordingly
     # if length(payload.func_imports) == 1 do
--- lib/wasmtime/native.ex
+++ lib/wasmtime/native.ex
@@ -7,6 +7,7 @@ defmodule Wasmtime.Native do
   def exports(_id), do: :erlang.nif_error(:nif_not_loaded)
   def func_call(_id, _func, _params), do: :erlang.nif_error(:nif_not_loaded)
   def func_exports(_id), do: :erlang.nif_error(:nif_not_loaded)
-  def load_from_file(_id, _file_name), do: :erlang.nif_error(:nif_not_loaded)
-  def load_from_bytes(_id, _bytes), do: :erlang.nif_error(:nif_not_loaded)
+
+  def load_from(_id, _file_name, _bin, _func_imports),
+    do: :erlang.nif_error(:nif_not_loaded)
 end
